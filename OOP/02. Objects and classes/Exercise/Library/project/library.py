dd707a79c78af93cf8f1a2ec48f477ceb4e8ab00
Dimitar Dimitrov <dimitar.d.professional@gmail.com>
2023-03-04 17:17:12 +0200

    Python-Advanced Materials

---
+++ OOP/02. Objects and classes/Exercise/Library/project/library.py
@@ -0,0 +1,36 @@
+from typing import List, Dict
+
+from project.user import User
+
+
+class Library:
+    def __init__(self):
+        self.user_records: List[User] = []
+        self.books_available: Dict[str, List[str]] = {}  # { author: [book1, book2...] }
+        self.rented_books: Dict[str, Dict[str: int]] = {}  # { username: { book_name: days_to_return } }
+
+    def get_book(self, author: str, book_name: str, days_to_return: int, user: User) -> str:
+        if book_name in self.books_available[author]:  # the book is available
+            self.books_available[author].remove(book_name)
+            user.books.append(book_name)
+
+            # I've made a mistake here by not checking if the username is already in the rented_books
+            if user.username in self.rented_books:
+                self.rented_books[user.username][book_name] = days_to_return
+            else:
+                self.rented_books[user.username] = {book_name: days_to_return}
+
+            return f"{book_name} successfully rented for the next {days_to_return} days!"
+
+        # the book is already rented
+        return f'The book "{book_name}" is already rented and will be available in {self.rented_books[user.username][book_name]} days!'
+
+    def return_book(self, author: str, book_name: str, user: User) -> str:
+        if book_name in user.books:  # he has the book in his records
+            user.books.remove(book_name)
+
+            self.books_available[author].append(book_name)
+            self.rented_books[user.username].pop(book_name)
+
+        else:
+            return f"{user.username} doesn't have this book in his/her records!"
---
+++ OOP/02. Objects and classes/Exercise/Library/project/registration.py
@@ -0,0 +1,30 @@
+from project.library import Library
+from project.user import User
+
+
+class Registration:
+    def add_user(self, user: User, library: Library):
+        if user not in library.user_records:
+            library.user_records.append(user)
+        else:
+            return f"User with id = {user.user_id} already registered in the library!"
+
+    def remove_user(self, user: User, library: Library):
+        if user in library.user_records:
+            library.user_records.remove(user)
+        else:
+            return "We could not find such user to remove!"
+
+    def change_username(self, user_id: int, new_username: str, library: Library):
+
+        for test_user in library.user_records:
+            if test_user.user_id == user_id:
+                if test_user.username != new_username:
+                    test_user.username = new_username
+                    return f"Username successfully changed to: {new_username} for user id: {user_id}"
+
+                else:
+                    return "Please check again the provided username - it should be different than the username used so far!"
+
+        # I've made a mistake here by adding else: before the return
+        return f"There is no user with id = {user_id}!"
